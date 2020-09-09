47f0d5a5e70e2b149212f649cac22cc9e2764420
Kenneth Lawson <koodog@gmai.com>
2020-09-08 23:38:46 -0400

    Changed airport to reflect one that is closer to me.

--- xmobar/.config/xmobar.hs
+++ xmobar/.config/xmobar.hs
@@ -38,7 +38,7 @@ Config {
    , commands =
 
         -- weather monitor
-        [ Run Weather "RJTT" [ "--template", "<skyCondition> | <fc=#4682B4><tempC></fc>°C | <fc=#4682B4><rh></fc>% | <fc=#4682B4><pressure></fc>hPa"
+        [ Run Weather "KBGM" [ "--template", "<skyCondition> | <fc=#4682B4><tempF></fc>°F | <fc=#4682B4><rh></fc>% | <fc=#4682B4><pressure></fc>hPa"
                              ] 36000
 
         -- network activity monitor (dynamic interface resolution)
