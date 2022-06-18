3e62e994b464feaa5b3a03e0fac6f14a2f6486b1
Kenneth Lawson <koodog@gmail.com>
2022-06-18 01:44:27 -0400

    started patching dwm

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,1 +1,14 @@
 #! /bin/bash
+
+/usr/bin/emacs --daemon &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+feh --bg-scale ~/.config/SunDracula.jpg &
+pamac-tray &
+clipit &
+blueman-applet &
+xautolock -time 10 -locker "betterlockscreen -l" &
+/opt/piavpn/bin/pia-client &
+dropbox &
+picom &
+redshift &
+solaar -w hide &
