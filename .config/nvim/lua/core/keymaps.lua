724d2aaabce6e1738837c8482d729a6c3b059c03
Kenneth Lawson <koodog@gmail.com>
2022-12-02 16:17:41 -0500

    some updates and cleaned wallpapers

---
+++ .config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,20 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+-- use spaces for tas
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.shiftround = true
+vim.expandtab = true
+
+vim.keymap.set('n' , '<leader>h', 'nohlsearch<CR>')
+
+-- relitive line number
+vim.wo.relativenumber = true
