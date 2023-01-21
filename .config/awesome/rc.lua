4712c956377ecf8b55d99c5f6170b4d3c880ba11
the-guy1 <koodog@gmail.com>
2023-01-21 10:50:11 -0500

    working with Awesome because I want to use more lua code

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -46,10 +46,11 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+gears.wallpaper.maximized("~/.dotfiles/.config/tokyonight.svg")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "alacritty"
-editor = os.getenv("EDITOR") or "emacs"
+editor = os.getenv("EDITOR") or "nvim"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
@@ -490,7 +491,7 @@ awful.rules.rules = {
 
     -- Add titlebars to normal clients and dialogs
     { rule_any = {type = { "normal", "dialog" }
-      }, properties = { titlebars_enabled = true }
+      }, properties = { titlebars_enabled = false}
     },
 
     -- Set Firefox to always map on the tag named "2" on screen 1.
