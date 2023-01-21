c14f7d83ba0b3a5c76ad6add92b4074ab9681e77
the-guy1 <koodog@gmail.com>
2023-01-21 12:07:55 -0500

    trying catppuccin again

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,4 +1,4 @@
 vim.o.termguicolors = true
-vim.cmd.colorscheme("tokyonight")
+vim.cmd.colorscheme("catppuccin")
 -- vim.api.nvim_set_h1(0, "Normal", { bg = "none" })
 -- vim.api.nvim_set_h1(0, "NormalFloat", { bg = "none" })
