727e05a5571b916533450bce4a4086562e233a22
the-guy1 <koodog@gmail.com>
2023-01-25 11:15:10 -0500

    Added Tmux config and other files that didn't get added before

---
+++ .config/nvim/after/plugin/whichkey.lua
@@ -0,0 +1,11 @@
+local status, which_key = pcall(require, "which-key")
+if not status then
+	return
+end
+
+which_key.setup({
+	-- your configuration comes here
+	-- or leave it empty to use the default settings
+})
+
+
