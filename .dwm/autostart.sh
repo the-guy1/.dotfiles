2792e5add93934e3294ebaece0f9c92fb9bfb142
the-guy1 <koodog@gmail.com>
2023-02-13 12:41:44 -0500

    added dwm autostart

---
+++ .dwm/autostart.sh
@@ -0,0 +1,16 @@
+/usr/bin/emacs --daemon &
+nm-applet &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+feh --bg-scale ~/.dotfiles/.config/wallpaper/tokyo-night36.svg &
+xfce-power-manager &
+pamac-tray &
+clipit &
+blueman-applet &
+xautolock -time 10 -locker "betterlockscreen -l" &
+/opt/piavpn/bin/pia-client &
+dropbox &
+picom &
+caffeine &
+redshift &
+solaar -w hide &
+exec slstatus &
